ef633b24c47060e96b6f4e7ce0b08c16822ed8f1
Thierry Almeida <thierryluiz@hotmail.com>
2021-05-22 23:19:38 -0300

    POST, insert new Client

--- backend/src/main/java/com/devsuperior/capitulo1/crudcompleto/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/capitulo1/crudcompleto/resources/ClientResource.java
@@ -1,19 +1,21 @@
 package com.devsuperior.capitulo1.crudcompleto.resources;
 
 import java.io.Serializable;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.capitulo1.crudcompleto.dto.ClientDTO;
 import com.devsuperior.capitulo1.crudcompleto.services.ClientService;
@@ -44,8 +46,10 @@ public class ClientResource implements Serializable{
 		return ResponseEntity.ok(dto);
 	}
 	
-	@PutMapping
-	public ResponseEntity<ClientDTO> insert(@PathVariable ClientDTO client){
-		return null;
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO client){
+		client = service.insert(client);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(client.getId()).toUri();
+		return ResponseEntity.created(uri).body(client);
 	}
 }
