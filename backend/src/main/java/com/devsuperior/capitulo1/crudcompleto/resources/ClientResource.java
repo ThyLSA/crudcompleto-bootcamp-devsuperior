db0f110b861fa0172eccb2ae78bdc21b98da064b
Thierry Almeida <thierryluiz@hotmail.com>
2021-05-22 22:43:02 -0300

    Find all paged

--- backend/src/main/java/com/devsuperior/capitulo1/crudcompleto/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/capitulo1/crudcompleto/resources/ClientResource.java
@@ -1,12 +1,16 @@
 package com.devsuperior.capitulo1.crudcompleto.resources;
 
 import java.io.Serializable;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +27,14 @@ public class ClientResource implements Serializable{
 	ClientService service;
 	
 	@GetMapping
-	public ResponseEntity<List<ClientDTO>> findAll(){
-		List<ClientDTO> list = service.findAll();
+	public ResponseEntity<Page<ClientDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "10") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+			){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Page<ClientDTO> list = service.findAllPaged(pageRequest);
 		return ResponseEntity.ok(list);
 	}
 	
@@ -32,5 +42,10 @@ public class ClientResource implements Serializable{
 	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
 		ClientDTO dto = service.findbyId(id);
 		return ResponseEntity.ok(dto);
+	}
+	
+	@PutMapping
+	public ResponseEntity<ClientDTO> insert(@PathVariable ClientDTO client){
+		return null;
 	}
 }
--- backend/src/main/java/com/devsuperior/capitulo1/crudcompleto/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/capitulo1/crudcompleto/services/ClientService.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,8 +29,35 @@ public class ClientService implements Serializable{
 	}
 	
 	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(Pageable pageable){
+		Page<Client> page = repository.findAll(pageable);
+		return page.map(x -> new ClientDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
 	public ClientDTO findbyId(Long id) {
 		Client entity = repository.findById(id).get();
 		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO insert(ClientDTO client) {
+		Client entity = setAttributes(client);
+		return new ClientDTO(repository.save(entity));
+	}
+	
+	public ClientDTO update(ClientDTO client) {
+		Client entity = repository.getOne(client.getId());
+		return null;
+	}
+	
+	private Client setAttributes(ClientDTO dto) {
+		Client entity = new Client();
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		entity.setIncome(dto.getIncome());
+		return entity;
 	}
 }
